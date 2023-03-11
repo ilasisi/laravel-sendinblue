36b1c44cf34935177f1e65237655febf01b59d89
Ibrahim Lasisi <ilasisi90@gmail.com>
2023-03-11 16:24:42 +0100

    get all contacts

--- src/SendInBlue.php
+++ src/SendInBlue.php
@@ -2,10 +2,62 @@
 
 namespace Ilasisi\SendInBlue;
 
+use Exception;
+use SendinBlue\Client\Api\ContactsApi;
+use SendinBlue\Client\Configuration;
+
 class SendInBlue
 {
-    public static function checkIfWorks()
+    /**
+     * SendInBlue API Key
+     *
+     * @var string
+     */
+    public $apiKey;
+
+    /**
+     * SendInBlue config
+     *
+     * @var Configuration
+     */
+    public $config;
+
+     /**
+     * SendInBlue Account Instance
+     *
+     * @var ContactsApi
+     */
+    public $contactInstance;
+
+    public function __construct()
     {
-        echo 'it works';
+        $this->setApiKey();
+        $this->setConfig();
+        $this->setContactInstance();
+    }
+
+    public function setApiKey(): void
+    {
+        $this->apiKey = config('sendinblue.api_key');
+    }
+
+    public function setConfig(): void
+    {
+        $this->config = Configuration::getDefaultConfiguration()
+            ->setApiKey('api-key', $this->apiKey);
+    }
+
+    public function getContacts(): \SendinBlue\Client\Model\GetContacts
+    {
+        try {
+            return $this->contactInstance->getContacts();
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    public function setContactInstance(): void
+    {
+        $this->contactInstance = new ContactsApi(config: $this->config);
     }
 }
